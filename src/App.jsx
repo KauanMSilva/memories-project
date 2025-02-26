f452f2388bf1c864b1a80bc4ca0c8dbd047ff56a
Kauan Mariano Silva <144391114+KauanMSilva@users.noreply.github.com>
2025-02-25 21:59:11 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import NewMemory from "./pages/new-memory";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/new-memory" element={<NewMemory />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
